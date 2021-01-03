a451fcfa8b801f2c60cec10a923eed93ab7539df
Navneet Kaur Keer <navneetkaur200001@gmail.com>
2021-01-03 06:43:26 +0530

    Input Output Processing

--- app.js
+++ app.js
@@ -1,11 +1,9 @@
 var btnTranslate = document.querySelector("#btn-translate");
 var txtInput = document.querySelector("#txt-input");
-
-console.log(txtInput)
+var outputDiv = document.querySelector("#output");
 
 function clickHandler() {
-    console.log("Clicked!");
-    console.log("Input",txtInput.value);
+    outputDiv.innerText = "bcdbchdbcdbk" + txtInput.nodeValue;
 };
 
 btnTranslate.addEventListener("click", clickHandler)
