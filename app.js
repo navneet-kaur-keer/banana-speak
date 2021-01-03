1c8cef45a9b6c0847a5fa134bd0a6a6f0d5c261b
Navneet Kaur Keer <navneetkaur200001@gmail.com>
2021-01-03 07:54:13 +0530

    Defined serverURL and Error Handler

--- app.js
+++ app.js
@@ -2,8 +2,27 @@ var btnTranslate = document.querySelector("#btn-translate");
 var txtInput = document.querySelector("#txt-input");
 var outputDiv = document.querySelector("#output");
 
+var serverURL = "https://api.funtranslations.com/translate/minion.json"
+
+function getTranslationURL(text) {
+    return serverURL + "?" + "text=" + text
+}
+
+function errorHandler(error) {
+    console.log("error occured", error)
+    alert("Something is wrong with server. Try Again next time.")
+}
+
 function clickHandler() {
-    outputDiv.innerText = "bcdbchdbcdbk" + txtInput.nodeValue;
+    var inputText = txtInput.value; //taking input
+
+    fetch(getTranslationURL(inputText)) //calling server for processing
+        .then(response => response.json())
+        .then(json => {
+            var translatedText = json.contents.translated;
+            outputDiv.innerText = translatedText; //output
+        })
+        .catch(errorHandler)
 };
 
 btnTranslate.addEventListener("click", clickHandler)
